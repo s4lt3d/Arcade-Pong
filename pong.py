83261ee39faf1df6dc88718f2e4dcfb6d5a14106
Walter <walter.gordy@gmail.com>
2019-05-14 15:27:56 -0600

    Paddles move with collision

--- pong.py
+++ pong.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon May 13 23:28:06 2019
-@author: walte
+@author: walter
 """
-
 import arcade
 import numpy as np
-
 
 SCREEN_WIDTH = 320
 SCREEN_HEIGHT = 240
 SCREEN_TITLE = "Pong"
 
-MOVEMENT_SPEED = 3
+MOVEMENT_SPEED = 2
+PADDLE_MOVEMENT_SPEED = 1.5
+
+
+PLAYER1_UP = arcade.key.W
+PLAYER1_DOWN = arcade.key.S
+
+
+PLAYER2_UP = arcade.key.UP
+PLAYER2_DOWN = arcade.key.DOWN
+
 
 class Paddle:
-    def __init__(self, position_x, position_y, change_x, change_y, width, height, color, player_numer = 1):
+    def __init__(self, position_x, position_y, change_x, change_y, width, height, color, player_number = 1):
 
         # Take the parameters of the init function above, and create instance variables out of them.
         self.position_x = position_x
@@ -25,26 +33,29 @@ class Paddle:
         self.width = width
         self.height = height
         self.color = color
-                
+        self.player_number = player_number
+        
+        if player_number == 1:
+            self.up_key = PLAYER1_UP
+            self.down_key = PLAYER1_DOWN
+        else:
+            self.up_key = PLAYER2_UP
+            self.down_key = PLAYER2_DOWN
+        
     def draw(self):
         """ Draw the balls with the instance variables we have. """
         arcade.draw_rectangle_filled(self.position_x, self.position_y, self.width, self.height, self.color)
         
     def on_key_press(self, key, modifiers):
-        if key == arcade.key.LEFT:
-            self.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.change_y = -MOVEMENT_SPEED
+
+        if key == self.up_key:
+            self.change_y = PADDLE_MOVEMENT_SPEED
+        elif key == self.down_key:
+            self.change_y = -PADDLE_MOVEMENT_SPEED
             
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.change_x = 0
-        elif key == arcade.key.UP or key == arcade.key.DOWN:
+        if key == self.up_key or key == self.down_key:
             self.change_y = 0
 
     def update(self):
@@ -61,54 +72,59 @@ class Paddle:
         self.bottom_left_y = self.position_y - self.height/2
         
 class Ball:
-    def __init__(self, position_x, position_y, change_x, change_y, radius, color):
+    def __init__(self, position_x, position_y, change_x, change_y, width, color):
 
         # Take the parameters of the init function above, and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
         self.change_x = change_x
         self.change_y = change_y
-        self.radius = radius
+        self.width = width
         self.color = color
+        self.collision = False
 
     def draw(self):
         """ Draw the balls with the instance variables we have. """
-        arcade.draw_rectangle_filled(self.position_x, self.position_y, self.radius, self.radius, self.color)
+        arcade.draw_rectangle_filled(self.position_x, self.position_y, self.width, self.width, self.color)
 
     def update(self):
         # Move the ball
 
         # See if the ball hit the edge of the screen. If so, change direction
-        if self.position_x < self.radius:
-            self.position_x = self.radius
+        if self.collision == True:
+            self.change_x *= -1
+            
+        if self.position_x < self.width/2:
+            self.position_x = self.width/2
             self.change_x *= -1
 
-        if self.position_x > SCREEN_WIDTH - self.radius:
-            self.position_x = SCREEN_WIDTH - self.radius
+        if self.position_x > SCREEN_WIDTH - self.width/2:
+            self.position_x = SCREEN_WIDTH - self.width/2
             self.change_x *= -1
 
-        if self.position_y < self.radius:
-            self.position_y = self.radius
+        if self.position_y < self.width/2:
+            self.position_y = self.width/2
             self.change_y *= -1
 
-        if self.position_y > SCREEN_HEIGHT - self.radius:
-            self.position_y = SCREEN_HEIGHT - self.radius
+        if self.position_y > SCREEN_HEIGHT - self.width/2:
+            self.position_y = SCREEN_HEIGHT - self.width/2
             self.change_y *= -1
             
-        self.position_y += self.change_y
-        self.position_x += self.change_x
+        self.position_y += MOVEMENT_SPEED * self.change_y
+        self.position_x += MOVEMENT_SPEED * self.change_x
         
-        self.top_right_y = self.position_y + self.radius
-        self.top_right_x = self.position_x + self.radius
-        self.bottom_left_x = self.position_x - self.radius
-        self.bottom_left_y = self.position_y - self.radius
+        self.top_right_y = self.position_y + self.width/2
+        self.top_right_x = self.position_x + self.width/2
+        self.bottom_left_x = self.position_x - self.width/2
+        self.bottom_left_y = self.position_y - self.width/2
         
     def check_collision(self, other):
-        return not (self.top_right_x < other.bottom_left_x or 
-                    self.bottom_left_x > other.top_right_x or 
-                    self.top_right_y < other.bottom_left_y or 
-                    self.bottom_left_y > other.top_right_y)
-            
+        
+        self.collision =  not (self.top_right_x < other.bottom_left_x or 
+                               self.bottom_left_x > other.top_right_x or 
+                               self.top_right_y < other.bottom_left_y or 
+                               self.bottom_left_y > other.top_right_y)
+        return self.collision
 
 class MyGame(arcade.Window):
 
@@ -124,7 +140,7 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.ASH_GREY)
 
         # Create our ball
-        self.ball = Ball(50, 50, 1, 1, 15, arcade.color.WHITE)
+        self.ball = Ball(50, 50, 1, 1, 32, arcade.color.WHITE)
         self.paddle1 = Paddle(8, 120, 0, 0, 16, 48, arcade.color.BLUE, 1)
         self.paddle2 = Paddle(SCREEN_WIDTH - 8, 120, 0, 0, 16, 48, arcade.color.RED, 2)
 
@@ -136,7 +152,7 @@ class MyGame(arcade.Window):
         self.paddle2.draw()
         arcade.draw_text("12", 10, SCREEN_HEIGHT-25, color=arcade.color.WHITE, font_name="COURIER NEW", font_size=20)
         arcade.draw_text("12", SCREEN_WIDTH - 50, SCREEN_HEIGHT-25, color=arcade.color.WHITE, font_name="COURIER NEW", font_size=20)
-        if self.ball.check_collision(self.paddle1):
+        if self.ball.check_collision(self.paddle1) or self.ball.check_collision(self.paddle2):
             arcade.draw_text("Collision", 50, SCREEN_HEIGHT-85, color=arcade.color.WHITE, font_name="COURIER NEW", font_size=20)    
 
     def update(self, delta_time):
