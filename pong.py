6785236edb960746923c8e7efe394c4573b33b1f
Walter <walter.gordy@gmail.com>
2019-05-14 09:59:31 -0600

    Changing to sprites for collision detection

--- pong.py
+++ pong.py
@@ -14,9 +14,10 @@ SCREEN_TITLE = "Pong"
 MOVEMENT_SPEED = 3
 
 
-class Paddle:
+class Paddle(arcade.Sprite):
     def __init__(self, position_x, position_y, change_x, change_y, width, height, color):
-
+        
+        super().__init__()
         # Take the parameters of the init function above, and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
@@ -25,10 +26,28 @@ class Paddle:
         self.width = width
         self.height = height
         self.color = color
+        
 
     def draw(self):
         """ Draw the balls with the instance variables we have. """
         arcade.draw_rectangle_filled(self.position_x, self.position_y, self.width, self.height, self.color)
+        
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT:
+            self.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.change_x = MOVEMENT_SPEED
+        elif key == arcade.key.UP:
+            self.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.change_y = -MOVEMENT_SPEED
+            
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.change_x = 0
+        elif key == arcade.key.UP or key == arcade.key.DOWN:
+            self.change_y = 0
 
     def update(self):
         # Move the paddle
@@ -40,7 +59,9 @@ class Paddle:
         
 class Ball:
     def __init__(self, position_x, position_y, change_x, change_y, radius, color):
-
+        
+        super().__init__()
+        
         # Take the parameters of the init function above, and create instance variables out of them.
         self.position_x = position_x
         self.position_y = position_y
@@ -55,11 +76,27 @@ class Ball:
 
     def update(self):
         # Move the ball
+
+        # See if the ball hit the edge of the screen. If so, change direction
+        if self.position_x < self.radius:
+            self.position_x = self.radius
+            self.change_x *= -1
+
+        if self.position_x > SCREEN_WIDTH - self.radius:
+            self.position_x = SCREEN_WIDTH - self.radius
+            self.change_x *= -1
+
+        if self.position_y < self.radius:
+            self.position_y = self.radius
+            self.change_y *= -1
+
+        if self.position_y > SCREEN_HEIGHT - self.radius:
+            self.position_y = SCREEN_HEIGHT - self.radius
+            self.change_y *= -1
+            
         self.position_y += self.change_y
         self.position_x += self.change_x
 
-        self.position_x = np.clip(self.position_x, self.radius, SCREEN_WIDTH - self.radius)
-        self.position_y = np.clip(self.position_y, self.radius, SCREEN_HEIGHT - self.radius)
 
 class MyGame(arcade.Window):
 
@@ -75,7 +112,7 @@ class MyGame(arcade.Window):
         arcade.set_background_color(arcade.color.ASH_GREY)
 
         # Create our ball
-        self.ball = Ball(50, 50, 0, 0, 15, arcade.color.AUBURN)
+        self.ball = Ball(50, 50, 1, 1, 15, arcade.color.AUBURN)
         self.paddle = Paddle(100, 100, 0, 0, 15, 15, arcade.color.BLUE)
 
     def on_draw(self):
@@ -90,21 +127,12 @@ class MyGame(arcade.Window):
 
     def on_key_press(self, key, modifiers):
         """ Called whenever the user presses a key. """
-        if key == arcade.key.LEFT:
-            self.ball.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.RIGHT:
-            self.ball.change_x = MOVEMENT_SPEED
-        elif key == arcade.key.UP:
-            self.ball.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.DOWN:
-            self.ball.change_y = -MOVEMENT_SPEED
+        self.paddle.on_key_press(key, modifiers)
+        
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
-        if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.ball.change_x = 0
-        elif key == arcade.key.UP or key == arcade.key.DOWN:
-            self.ball.change_y = 0
+        self.paddle.on_key_release(key, modifiers)
 
 
 def main():
